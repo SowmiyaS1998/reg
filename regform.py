d5ddeb2e1519cb5dd5c16b815b4632ea502331fa
Sowmiya S <83083609+SowmiyaS1998@users.noreply.github.com>
2021-06-04 18:31:51 +0530

    Update regform.py
    updated registeration form

--- regform.py
+++ regform.py
@@ -2,6 +2,7 @@
 from flask import Flask,render_template,request
 import mysql.connector
 
+
 application = Flask(__name__)
 
 @application.route("/")
@@ -21,7 +22,18 @@ def result():
     mycursor=mydb.cursor()
     if request.method=="POST":
         signup=request.form
-
+        Name=signup['name']
+        Class=signup['class']
+        Math_mark=signup['math']
+        Science_mark=signup['science']
+        Social_mark=signup['social']
+        English_mark=signup['english']
+        Tamil_mark=signup['tamil']
+        
+        mycursor.execute("insert into reg(name,class,math,science,social,english,tamil) values(%s,%s,%s,%s,%s,%s,%s)",(Name,Class,Math_mark,Science_mark,Social_mark,English_mark,Tamil_mark))
+        mydb.commit()
+        mycursor.close()
+        return "Registered Successfully"
 #pass this if this is the main function
 if __name__ == '__main__':
   application.run(debug=True)
