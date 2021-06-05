cda9bdbc58851f90526d2cca3b1f7bd2aa265a2a
Sowmiya S <83083609+SowmiyaS1998@users.noreply.github.com>
2021-06-05 10:18:00 +0530

    Update regform.py

--- regform.py
+++ regform.py
@@ -5,12 +5,12 @@ import mysql.connector
 
 application = Flask(__name__)
 
-@application.route("/")
+@application.route('/')
 
 def student():
     return render_template('reg.html')
 
-@application.route("/result",methods=['POST','GET'])
+@application.route('/result',methods=['POST','GET'])
 def result():
     mydb=mysql.connector.connect(
         host="localhost",
@@ -20,7 +20,7 @@ def result():
     )
 
     mycursor=mydb.cursor()
-    if request.method=="POST":
+    if request.method=='POST':
         signup=request.form
         Name=signup['name']
         Class=signup['class']
@@ -30,7 +30,7 @@ def result():
         English_mark=signup['english']
         Tamil_mark=signup['tamil']
         
-        mycursor.execute("insert into reg(name,class,math,science,social,english,tamil) values(%s,%s,%s,%s,%s,%s,%s)",(Name,Class,Math_mark,Science_mark,Social_mark,English_mark,Tamil_mark))
+        mycursor.execute("insert into reg(name,class,math,science,social,english,tamil)values(%s,%s,%s,%s,%s,%s,%s)",(Name,Class,Math_mark,Science_mark,Social_mark,English_mark,Tamil_mark))
         mydb.commit()
         mycursor.close()
         return "Registered Successfully"
